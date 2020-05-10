9ed3e29c7d4cf1f5e76f011569fa514d229272a4
Everton Sávio S. Lucas <everluca@outlook.com>
2020-05-10 17:08:19 -0300

    Primeiro Commit

---
+++ KanbanFullStack/src/main/java/evertonsavio/dev/kanbanfullstack/KanbanfullstackApplication.java
@@ -0,0 +1,13 @@
+package evertonsavio.dev.kanbanfullstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KanbanfullstackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KanbanfullstackApplication.class, args);
+	}
+
+}
---
+++ KanbanFullStack/src/test/java/evertonsavio/dev/kanbanfullstack/KanbanfullstackApplicationTests.java
@@ -0,0 +1,13 @@
+package evertonsavio.dev.kanbanfullstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KanbanfullstackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

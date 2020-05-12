2e895f62c77c5ab29e6051363e07dc9ec26b9159
Everton Sávio S. Lucas <everluca@outlook.com>
2020-05-12 18:11:40 -0300

    GetById

--- KanbanFullStack/src/main/java/evertonsavio/dev/kanbanfullstack/services/ProjectService.java
+++ KanbanFullStack/src/main/java/evertonsavio/dev/kanbanfullstack/services/ProjectService.java
@@ -1,6 +1,7 @@
 package evertonsavio.dev.kanbanfullstack.services;
 
 import evertonsavio.dev.kanbanfullstack.domain.Project;
+import evertonsavio.dev.kanbanfullstack.exceptions.ProjectIdException;
 import evertonsavio.dev.kanbanfullstack.repositories.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,9 +14,24 @@ public class ProjectService {
 
     public Project saveOrUpdateProject(Project project){
 
-        //Logic
+        try{
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            return projectRepository.save(project);
+        }catch (Exception e){
+            throw new ProjectIdException("Project ID " + project.getProjectIdentifier().toUpperCase()+" Already existis");
+        }
 
-        return projectRepository.save(project);
+
     }
 
+    public Project findProjectByIdentifier(String projectId){
+
+        Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
+
+        if (project == null){
+            throw new ProjectIdException("Project "+ projectId +" does not existis");
+        }
+
+        return project;
+    }
 }
--- KanbanFullStack/src/main/java/evertonsavio/dev/kanbanfullstack/web/ProjectController.java
+++ KanbanFullStack/src/main/java/evertonsavio/dev/kanbanfullstack/web/ProjectController.java
@@ -1,16 +1,14 @@
 package evertonsavio.dev.kanbanfullstack.web;
 
 import evertonsavio.dev.kanbanfullstack.domain.Project;
+import evertonsavio.dev.kanbanfullstack.services.MapValidationErrorService;
 import evertonsavio.dev.kanbanfullstack.services.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import javax.xml.ws.Response;
@@ -25,22 +23,23 @@ public class ProjectController {
     @Autowired
     private ProjectService projectService;
 
+    @Autowired
+    private MapValidationErrorService mapValidationErrorService;
+
     //public ResponseEntity<Project> createNewProject(@Valid @RequestBody Project project, BindingResult result){
     @PostMapping("")
     public ResponseEntity<?> createNewProject(@Valid @RequestBody Project project, BindingResult result){
-        if (result.hasErrors()){
 
-            Map<String, String> errorMap = new HashMap<>();
+        ResponseEntity<?> errorMap = mapValidationErrorService.MapValidationService(result);
+        if(errorMap!=null) return errorMap;
 
-            for(FieldError error: result.getFieldErrors()){
-                errorMap.put(error.getField(), error.getDefaultMessage());
-            }
-
-
-            return new ResponseEntity<Map<String, String>>(errorMap, HttpStatus.BAD_REQUEST);
-        }
         Project project1 = projectService.saveOrUpdateProject(project);
         return new ResponseEntity<Project>(project, HttpStatus.CREATED);
+    }
+    @GetMapping("/{projectId}")
+    public ResponseEntity<?> getProjectByID(@PathVariable String projectId){
+        Project project = projectService.findProjectByIdentifier(projectId);
+        return new ResponseEntity<Project>(project, HttpStatus.OK);
     }
 
 }

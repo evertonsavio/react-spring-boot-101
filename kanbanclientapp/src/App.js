0f97655a10cc0e69b9c80d41fb976f3b6566616b
Everton Sávio S. Lucas <everluca@outlook.com>
2020-05-14 14:41:20 -0300

    Redux + Axios

--- kanbanclientapp/src/App.js
+++ kanbanclientapp/src/App.js
@@ -5,16 +5,23 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Layout/Header';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import AddProject from './components/Project/AddProject';
+import {Provider} from 'react-redux';
+import store from './store';
+
+//yarn add redux@4.0.0 react-redux@5.0.7 redux-thunk@2.3.0
+//yarn add axios@0.18.0
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Header />
-        <Route exact path="/dashboard" component={Dashboard} />
-        <Route exact path="/addProject" component={AddProject} />
-      </div>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <div className="App">
+          <Header />
+          <Route exact path="/dashboard" component={Dashboard} />
+          <Route exact path="/addProject" component={AddProject} />
+        </div>
+      </Router>
+    </Provider>
   );
 }
 

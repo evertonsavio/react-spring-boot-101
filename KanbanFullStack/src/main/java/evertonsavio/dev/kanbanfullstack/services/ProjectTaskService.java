f8bc5d29b17c1488dedc81f7ffe284bd521a63d7
Everton Sávio S. Lucas <everluca@outlook.com>
2020-05-30 10:56:15 -0300

    Postgres, runtime exceptions, backlog and serviceTasks

--- KanbanFullStack/src/main/java/evertonsavio/dev/kanbanfullstack/services/ProjectTaskService.java
+++ KanbanFullStack/src/main/java/evertonsavio/dev/kanbanfullstack/services/ProjectTaskService.java
@@ -2,10 +2,14 @@ package evertonsavio.dev.kanbanfullstack.services;
 
 import evertonsavio.dev.kanbanfullstack.domain.Backlog;
 import evertonsavio.dev.kanbanfullstack.domain.ProjectTask;
+import evertonsavio.dev.kanbanfullstack.exceptions.ProjectIdException;
+import evertonsavio.dev.kanbanfullstack.exceptions.ProjectNotFoundException;
 import evertonsavio.dev.kanbanfullstack.repositories.BacklogRepository;
 import evertonsavio.dev.kanbanfullstack.repositories.ProjectTaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ProjectTaskService {
@@ -18,10 +22,9 @@ public class ProjectTaskService {
     private ProjectTaskRepository projectTaskRepository;
 
 
-    public ProjectTask addProjectTask(String projectIdentifier, ProjectTask projectTask){
+    public ProjectTask addProjectTask(String projectIdentifier, ProjectTask projectTask) {
 
-        //Exceptions: Project not found
-
+        try {
         //PTs to be added to a specific project, project != null, BL exists
         Backlog backlog = backlogRepository.findByProjectIdentifier(projectIdentifier);
         //set the bl to pt
@@ -34,20 +37,27 @@ public class ProjectTaskService {
         backlog.setPTSequence(BacklogSequence);
 
         //Add Sequence to Project Task
-        projectTask.setProjectSequence(backlog.getProjectIdentifier()+"-"+BacklogSequence);
+        projectTask.setProjectSequence(backlog.getProjectIdentifier() + "-" + BacklogSequence);
         projectTask.setProjectIdentifier(projectIdentifier);
 
         //INITIAL priority when priority null
 
         //INITIAL status when status is null
-        if(projectTask.getStatus()==""|| projectTask.getStatus()==null){
+        if (projectTask.getStatus() == "" || projectTask.getStatus() == null) {
             projectTask.setStatus("TO_DO");
         }
 
-        if(projectTask.getPriority()==null){ //In the future we need projectTask.getPriority()== 0 to handle the form
+        if (projectTask.getPriority() == null) { //In the future we need projectTask.getPriority()== 0 to handle the form
             projectTask.setPriority(3);
         }
 
         return projectTaskRepository.save(projectTask);
+    }catch (Exception e){
+            throw new ProjectNotFoundException("Project not found");
+        }
+    }
+
+    public Iterable<ProjectTask> findBacklogById(String id) {
+        return projectTaskRepository.findByProjectIdentifierOrderByPriority(id);
     }
 }

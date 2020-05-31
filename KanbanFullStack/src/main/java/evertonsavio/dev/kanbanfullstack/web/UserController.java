2fe063baca01c21a759fb3ef5089081658f9fed5
Everton Sávio S. Lucas <everluca@outlook.com>
2020-05-31 16:44:41 -0300

    JWT Token Provider

--- KanbanFullStack/src/main/java/evertonsavio/dev/kanbanfullstack/web/UserController.java
+++ KanbanFullStack/src/main/java/evertonsavio/dev/kanbanfullstack/web/UserController.java
@@ -1,16 +1,25 @@
 package evertonsavio.dev.kanbanfullstack.web;
 
 import evertonsavio.dev.kanbanfullstack.domain.User;
+import evertonsavio.dev.kanbanfullstack.payload.JWTLoginSucessReponse;
+import evertonsavio.dev.kanbanfullstack.payload.LoginRequest;
+import evertonsavio.dev.kanbanfullstack.security.JwtTokenProvider;
 import evertonsavio.dev.kanbanfullstack.services.MapValidationErrorService;
 import evertonsavio.dev.kanbanfullstack.services.UserService;
 import evertonsavio.dev.kanbanfullstack.validator.UserValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+
+import static evertonsavio.dev.kanbanfullstack.security.SecurityConstants.TOKEN_PREFIX;
 
 @RestController
 @RequestMapping("/api/users")
@@ -26,9 +35,32 @@ public class UserController {
     @Autowired
     private UserValidator userValidator;
 
+    @Autowired
+    private JwtTokenProvider jwtTokenProvider;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @PostMapping("/login")
+    public ResponseEntity<?> authenticateUser(@Valid @RequestBody LoginRequest loginRequest, BindingResult result){
+        ResponseEntity<?> errorMap = mapValidationErrorService.MapValidationService(result);
+        if(errorMap != null) return errorMap;
+
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        loginRequest.getUsername(),
+                        loginRequest.getPassword()
+                )
+        );
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String jwt = TOKEN_PREFIX + jwtTokenProvider.generateToken(authentication);
+
+        return ResponseEntity.ok(new JWTLoginSucessReponse(true, jwt));
+    }
+
     @PostMapping("/register")
     public ResponseEntity<?> registerUser(@Valid @RequestBody User user, BindingResult result){
-        //validar password
+        //validate password
         userValidator.validate(user, result);
 
         ResponseEntity<?> errorMap = mapValidationErrorService.MapValidationService(result);
